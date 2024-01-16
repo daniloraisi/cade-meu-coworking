a3ca66259f05add27634d28f02b80301de34548e
Maheus Magalhães <basemagalha@gmail.com>
2024-01-16 20:14:06 -0300

    Update dump.sql, criei a tabela workspaces.local

--- db/dump.sql
+++ db/dump.sql
@@ -1,9 +1,11 @@
+CREATE SCHEMA "workspaces";
+
 CREATE TABLE "users" (
   "username" varchar(40) UNIQUE PRIMARY KEY,
   "name" varchar(255),
   "email" varchar(255) UNIQUE,
   "friends" json,
-  "password" MD5(),
+  "password" md5(),
   "about" text,
   "birth" date,
   "fv_workspaces" json UNIQUE
@@ -16,3 +18,12 @@ CREATE TABLE "workspaces" (
   "about" text
 );
 
+CREATE TABLE "workspaces.local" (
+  "id" int,
+  "street" varchar(255),
+  "zipcode" varchar(12),
+  "latitude" decimal(12,9),
+  "longitude" decimal(12,9)
+);
+
+ALTER TABLE "workspaces.local" ADD FOREIGN KEY ("id") REFERENCES "workspaces" ("id");
