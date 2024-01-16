9bbe35bb1d6d4bd5874263342e0bb5734091a4e3
Maheus Magalhães <basemagalha@gmail.com>
2024-01-16 20:39:23 -0300

    Retirei a coluna local da tabela workspaces.

--- db/dump.sql
+++ db/dump.sql
@@ -1,34 +1,30 @@
-CREATE TABLE "users" (
-  "username" varchar(40) UNIQUE PRIMARY KEY,
-  "name" varchar(255),
-  "email" varchar(255) UNIQUE,
-  "password" md5(),
-  "about" text,
-  "birth" date,
-  "fv_workspaces" json UNIQUE
-);
+Table users{
+  username varchar(40) [pk, unique]
+  name varchar(255)
+  email varchar(255) [unique]
+  friends json
+  password md5
+  about text
+  birth date
+  fv_workspaces json [unique]
+}
 
-CREATE TABLE "workspaces" (
-  "id" int PRIMARY KEY,
-  "name" varchar(60) UNIQUE,
-  "local" varchar(255),
-  "about" text
-);
+Table workspaces{
+  id int [pk]
+  name varchar(60) [unique]
+  about text
+}
 
-CREATE TABLE "workspaces_addresses" (
-  "id" int,
-  "street" varchar(255),
-  "zipcode" varchar(12),
-  "latitude" decimal(12,9),
-  "longitude" decimal(12,9)
-);
+Table workspaces_addresses{
+  id int [ref: > workspaces.id]
+  street varchar(255)
+  zipcode varchar(12)
+  latitude decimal(12,9)
+  longitude decimal(12,9)
+}
 
-CREATE TABLE "users_relationships" (
-  "username1" varchar(40),
-  "username2" varchar(40),
-  "created_at" now
-);
-
-ALTER TABLE "workspaces_addresses" ADD FOREIGN KEY ("id") REFERENCES "workspaces" ("id");
-ALTER TABLE "users_relationships" ADD FOREIGN KEY ("username1") REFERENCES "users" ("username");
-ALTER TABLE "users_relationships" ADD FOREIGN KEY ("username2") REFERENCES "users" ("username");
+Table users_relationships{
+  username1 varchar(40) [ref: > users.username]
+  username2 varchar(40) [ref: > users.username]
+  created_at now()
+}
